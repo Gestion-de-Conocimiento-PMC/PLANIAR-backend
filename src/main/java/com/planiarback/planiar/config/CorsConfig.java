fe9285e51f2973e4e0b346c486e343f40d079c98
titoK1751 <juandiqa@hotmail.com>
2025-10-24 16:47:14 -0500

    Fix add missing import

--- src/main/java/com/planiarback/planiar/config/CorsConfig.java
+++ src/main/java/com/planiarback/planiar/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.planiarback.planiar.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
